ffe8a321500956cde41871c2f66cb95a25f5272b
Mauri <mauri.monzon91@gmail.com>
2024-02-12 20:01:40 -0300

    Peticion get

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hola")
+function getUsers() {
+  axios({
+    url: "https://jsonplaceholder.typicode.com/users",
+    method: "GET",
+    params: {
+      _limit: 3,
+    },
+  })
+    .then((res) => console.log(res.data))
+    .catch((err) => console.log(err));
+}
+
+getUsers()
